8100aade094bd2c06e24dfa5f0b3b88b0785815c
Deb <dfelsent@gmail.com>
2013-06-14 11:58:49 -0400

    moved autoflushing to development config instead of init file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Bptest::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  
+  # I added this in based on SO advice on 6/14/2013
+  config.autoflush_log = true  
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
