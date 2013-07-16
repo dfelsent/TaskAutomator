2ba3b61908ee82118bc5c6adcf2012b03218a4c7
Deb <dfelsent@gmail.com>
2013-07-15 23:46:58 -0400

    erased from boot.rb and added encoding to environment.rb

--- config/boot.rb
+++ config/boot.rb
@@ -6,6 +6,3 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
-
-Encoding.default_external = 'UTF-8'
-Encoding.default_internal = 'UTF-8'
--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Bptest::Application.initialize!
 
-#Encoding.default_external = Encoding::UTF_8
-#Encoding.default_internal = Encoding::UTF_8
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 
+#Encoding.default_external = 'UTF-8'
+#Encoding.default_internal = 'UTF-8'
